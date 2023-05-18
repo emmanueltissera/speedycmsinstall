2efe300e7b75fb38d236701d2d267e59fa9a0270
Emmanuel Tissera <emmanuel@luminary.com>
2023-05-18 21:05:23 +1000

    Updated path to sh file

--- scripts/azure/v12rc1/download-umbraco-v12rc1-demo-script.sh
+++ scripts/azure/v12rc1/download-umbraco-v12rc1-demo-script.sh
@@ -1,3 +1,3 @@
-wget https://www.speedycmsinstall.com/scripts/azure/create-umbraco-v12rc1-demo.sh
+wget https://www.speedycmsinstall.com/scripts/azure/v12rc1/create-umbraco-v12rc1-demo.sh
 chmod +x create-umbraco-v12rc1-demo.sh
 ./create-umbraco-v12rc1-demo.sh
